6cf6139f4758a669a4f50a232988feabdd48d54b
Cutesehyeoning <165887251+u0ahya5@users.noreply.github.com>
2025-04-13 00:25:02 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        // 피자 판 개수 i
         for(int i=1; i<=n; i++){
             answer = i;
+            // i판 시켰을 때 조각 수가 n명에게 나누어 떨어진다면
             if(i*6%n==0){
-                answer = i;
+                answer=i;
                 break;
             }
         }
@@ -13,4 +15,4 @@ class Solution {
 }
 
 // 12명이 피자를 시킨다면? 2판 -> 1조각
-// 100명이 피자를 시킨다면? 120판 -> 
+// 100명이 피자를 시킨다면? 50판 -> 3조각
