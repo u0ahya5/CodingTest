80e1aae0014a46f8d4e7c6c7a3fbfe64e233cbfe
이세현 <165887251+u0ahya5@users.noreply.github.com>
2025-09-16 17:14:17 +0900

    [Bronze V] Title: 개수 세기, Time: 184 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        int count = 0;
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int M = sc.nextInt();
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == M){
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
