6b8d896a5909f9cadf562d87f789bf9b1d34ee86
이세현 <165887251+u0ahya5@users.noreply.github.com>
2026-01-30 23:53:06 +0900

    [Bronze V] Title: WARBOY, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.print((B/A*3)*C);
+	}
+}
