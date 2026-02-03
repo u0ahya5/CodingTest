25aded2507040506ba57ce0fb9c5ed8708ca82f9
이세현 <165887251+u0ahya5@users.noreply.github.com>
2026-02-04 01:55:36 +0900

    [Bronze IV] Title: Quadrants, Time: 188 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+            double x = sc.nextDouble();
+		    double y = sc.nextDouble();
+			if(x>0 && y>0) {
+				System.out.println("Q1");
+			} else if(x<0 && y>0) {
+				System.out.println("Q2");
+			} else if(x<0 && y<0) {
+				System.out.println("Q3");
+			} else if(x>0 && y<0) {
+				System.out.println("Q4");
+			} else {
+				System.out.println("AXIS");
+			}
+		}
+	}
+}
