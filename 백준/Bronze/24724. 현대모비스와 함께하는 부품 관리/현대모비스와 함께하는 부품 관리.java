c9e0599307145318b0bead120c6f619141459422
이세현 <165887251+u0ahya5@users.noreply.github.com>
2026-02-04 02:28:54 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 940 ms, Memory: 176596 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i = 0; i<T; i++) {
+			int N = sc.nextInt();
+			
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println("Material Management " + (i+1));
+			
+			for(int j = 0; j<N; j++) {
+				int A1 = sc.nextInt();
+				int B1 = sc.nextInt();
+			}
+			System.out.println("Classification ---- End!");
+		}
+	}
+}
