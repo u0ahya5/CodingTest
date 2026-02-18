5cd78486f3c6ce70a0a4070bd1952e1cd51bfae2
이세현 <165887251+u0ahya5@users.noreply.github.com>
2026-02-19 00:43:00 +0900

    [Bronze V] Title: Metronome, Time: 180 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		double n = sc.nextDouble();
+		
+		System.out.print(n / 4);
+	}
+}
