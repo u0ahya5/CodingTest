2fd43f48cdf41f19c664c5b0dbf0aaba015e559b
LEE SE HYEON <165887251+u0ahya5@users.noreply.github.com>
2026-04-01 23:48:19 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int M = sc.nextInt();
+		int K = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println(M * C);
+	}
+}
