a71adfe75248fe9389605a5620aec4e158b18227
LEE SE HYEON <165887251+u0ahya5@users.noreply.github.com>
2026-04-10 23:48:44 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 188 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			
+			System.out.println(str);
+		}
+	}
+}
