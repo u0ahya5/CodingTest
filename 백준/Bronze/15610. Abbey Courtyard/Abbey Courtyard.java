d415b1497d5d924910b2f1d06e0292cb7a68643c
LEE SE HYEON <165887251+u0ahya5@users.noreply.github.com>
2026-04-16 12:07:58 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 176 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String agrs[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		System.out.print(Math.sqrt(a)*4);
+	}
+}
