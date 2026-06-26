660315dc7a974a2c38cf938c5ab709d22f672d34
LEE SE HYEON <165887251+u0ahya5@users.noreply.github.com>
2026-06-26 23:49:08 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,8 @@
+def solution(price, money, count):
+    answer = 0
+    total = 0
+    for i in range(1, count+1):
+        total += price*i
+    if money < total:
+        answer = total - money
+    return answer
