27a1960cb811700157335275d2381f09d81df663
LEE SE HYEON <165887251+u0ahya5@users.noreply.github.com>
2026-08-04 23:55:15 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 48.01 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        while(true){
+            int temp[] = arr.clone();
+            for(int i = 0; i<arr.length; i++){
+                if(arr[i] >= 50 && arr[i] % 2 == 0){
+                    arr[i] /= 2;
+                } else if(arr[i] < 50 && arr[i] % 2 != 0){
+                    arr[i] *= 2;
+                    arr[i] += 1;
+                }
+            }
+            if(Arrays.equals(arr, temp)){
+                return answer;
+            } else{
+                answer++;
+            }
+        }
+    }
+}
